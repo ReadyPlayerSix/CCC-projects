5946cb895af283d753f7593ce84f811e54119083
ReadyPlayerSix <cc.runtal@gmail.com>
2024-12-20 20:42:08 -0600

    feat: added template for initial functionality in the main.js file.  Added variables for planet drop down.  Added function names for calculateWeight and handleClickEvent.

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 // Write your JavaScript code here!
+var planets = [
+    ['Pluto', 0.06],
+    ['Neptune', 1.148],
+    ['Uranus', 0.917],
+    ['Saturn', 1.139],
+    ['Jupiter', 2.640],
+    ['Mars', 0.3895],
+    ['Moon', 0.1655],
+    ['Earth', 1],
+    ['Venus', 0.9032],
+    ['Mercury', 0.377],
+    ['Sun', 27.9]
+];
+
+function calculateWeight(weight, planetNamef) {
+    // Code to return the correct weight
+}
+
+function handleClickEvent(e) {
+    // declare variable called userWeight and assign the value of the user's weight
+    // declare variable called planetName and assign the name of the selected planet from the drop down
+}
