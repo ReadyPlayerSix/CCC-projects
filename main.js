0991d5c811ad98db727acbfee64e8b1a0a16e993
ReadyPlayerSix <cc.runtal@gmail.com>
2024-12-22 13:44:22 -0600

    feat: added additional comments with requirements and notes

--- main.js
+++ main.js
@@ -20,4 +20,20 @@ function calculateWeight(weight, planetNamef) {
 function handleClickEvent(e) {
     // declare variable called userWeight and assign the value of the user's weight
     // declare variable called planetName and assign the name of the selected planet from the drop down
+    // declare variable called result and assign the value of the new calculated weight
+    // write code to display the message shown in the screenshot
 }
+
+// set the #calculate-button element's onclick method to use the handleClickEvent function
+// make it look nice by attaching a style.css file to you index.html and writing some basic styling
+// feel free to add classes and id's to the HTML elements as you need
+// import a google font and use it for some or all of the text on your page
+
+// bonus challenge
+// reverse the drop down order so that the sun is first
+
+// personal notes
+// include filter options for showing drop down order in distance from the sun
+// include filter option for dwarf planets
+// inlcude menu for adding new planets, name and weight multiplier
+// try styling with a different style library such as Bulma or Materialize for a modern look
