d16cddfa5746092e4080f918ffb1a38d9740e29a
ReadyPlayerSix <cc.runtal@gmail.com>
2024-12-22 18:33:46 -0600

    refactor:properly integrate project files into main repository

---
+++ Web102/project-backup/README.md
@@ -0,0 +1,15 @@
+# Web102 - Astroweight Calculator
+
+## Overview
+Consider a scenario where you've been hired by NASA to build an internal tool. Their engineers would like a way to calculate the weight of their various instruments on different planets in our solar system.
+
+This information will be used by the engineers to plan out how much equipment can be transported while still being able to depart the destination planet.
+
+## Technologies Used
+- JavaScript
+- HTML
+- CSS
+- Git
+
+## Notes
+- Discovered that the node_options had to be removed if the project were to be tested using VS Code's integrated terminal.
---
+++ Web102/project-backup/main.js
@@ -0,0 +1,41 @@
+// Test change to verify repository structure
+
+// Write your JavaScript code here!
+var planets = [
+    ['Pluto', 0.06],
+    ['Neptune', 1.148],
+    ['Uranus', 0.917],
+    ['Saturn', 1.139],
+    ['Jupiter', 2.640],
+    ['Mars', 0.3895],
+    ['Moon', 0.1655],
+    ['Earth', 1],
+    ['Venus', 0.9032],
+    ['Mercury', 0.377],
+    ['Sun', 27.9]
+];
+
+function calculateWeight(weight, planetNamef) {
+    // Code to return the correct weight
+}
+
+function handleClickEvent(e) {
+    // declare variable called userWeight and assign the value of the user's weight
+    // declare variable called planetName and assign the name of the selected planet from the drop down
+    // declare variable called result and assign the value of the new calculated weight
+    // write code to display the message shown in the screenshot
+}
+
+// set the #calculate-button element's onclick method to use the handleClickEvent function
+// make it look nice by attaching a style.css file to you index.html and writing some basic styling
+// feel free to add classes and id's to the HTML elements as you need
+// import a google font and use it for some or all of the text on your page
+
+// bonus challenge
+// reverse the drop down order so that the sun is first
+
+// personal notes
+// include filter options for showing drop down order in distance from the sun
+// include filter option for dwarf planets
+// inlcude menu for adding new planets, name and weight multiplier
+// try styling with a different style library such as Bulma or Materialize for a modern look
